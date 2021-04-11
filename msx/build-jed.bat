8168269b378f722198aede5372925f5f244eaa28
Dean Netherton <dean.netherton@gmail.com>
2021-04-11 13:38:56 +1000

    CPLD: Updated build script to strip Location attribute

--- msx/build-jed.bat
+++ msx/build-jed.bat
@@ -12,6 +12,10 @@ echo.
 %CUPLPATH%\cupl.exe -ju %CUPLPATH%\cupl.dl %1.pld
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-move /Y %1.jed bin\ >NUL
+REM Removed the location entry - the TL866II Plus programmer seems to have loading file issue
+
+findstr /v /i /L /c:"Location" %1.jed >bin\%1.jed
+
+del %1.jed
 
 echo.
