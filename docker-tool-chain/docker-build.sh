aebd849859130f758dd3560e1343bd665acf647f
Dean Netherton <dean.netherton@gmail.com>
2021-07-24 17:26:58 +1000

    build: docker-tool-chain: added c-lang-format tool

--- docker-tool-chain/docker-build.sh
+++ docker-tool-chain/docker-build.sh
@@ -4,5 +4,5 @@ set -e
 
 source ./version.sh
 
-docker build -t vipoo/yellow-msx-rc2014-tool-chain:${VERSION} .
+docker build --progress plain -t vipoo/yellow-msx-rc2014-tool-chain:${VERSION} .
 docker tag vipoo/yellow-msx-rc2014-tool-chain:${VERSION} vipoo/yellow-msx-rc2014-tool-chain:latest
