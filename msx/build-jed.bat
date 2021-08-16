db3d2d9bbf6407591b018ab7f13776790bff5f24
Dean Netherton <dean.netherton@gmail.com>
2021-08-16 19:31:05 +1000

    turbocpu: pld: implemented cpu slow down logic for 31 cycles post iorq

--- msx/build-jed.bat
+++ msx/build-jed.bat
@@ -9,7 +9,9 @@ if not exist "bin" mkdir bin
 echo Compiling %1.jed from %1.pld
 echo.
 
-%CUPLPATH%\cupl.exe -f - -ju %CUPLPATH%\cupl.dl %1.pld
+%CUPLPATH%\cupl.exe
+
+%CUPLPATH%\cupl.exe -e -x -f - -ju %CUPLPATH%\cupl.dl %1.pld
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Removed the location entry - the TL866II Plus programmer seems to have loading file issue
@@ -17,6 +19,7 @@ REM Removed the location entry - the TL866II Plus programmer seems to have loadi
 findstr /v /i /L /c:"Location" %1.jed >bin\%1.jed
 
 move %1.doc bin\%1.doc
+move %1.mx bin\%1.mx
 
 del %1.jed
 
