8bc52ed9cc97be29f6ad8dbb1deab7a26db281d3
Dean Netherton <dean.netherton@gmail.com>
2021-12-24 14:32:16 +1100

    muflash: now can flash a file to the first 16K of onboard ROM

---
+++ msx/apps/muflash/arguments.c
@@ -0,0 +1,66 @@
+#include "arguments.h"
+#include "print.h"
+#include <stdbool.h>
+
+const char *pFileName;
+
+uint8_t abort_with_help() {
+  printf("Usage:  muflash <filename>\r\n\r\n"
+         "Flash the onboard ROM of the MSX MUSIC Module\r\n\r\n"
+         "  filename\r\n"
+         "    The file name of image to flash\r\n"
+         "\r\n");
+  exit(1);
+
+  return 255;
+}
+
+uint8_t arg_help_msg(const uint8_t i, const char **argv) {
+  const char *arg_switch = argv[i];
+  if (strcmp(arg_switch, "/h") != 0 && strcasecmp(arg_switch, "/help") != 0)
+    return i;
+
+  return abort_with_help();
+}
+
+uint8_t arg_file_name(const uint8_t i, const char **argv) {
+  if (argv[i][0] != '/') {
+    if (pFileName)
+      return abort_with_help();
+    pFileName = argv[i];
+    return i + 1;
+  }
+
+  return i;
+}
+
+uint8_t abort_with_invalid_arg_msg(const uint8_t i, const char **argv) {
+  printf("Invalid command line option '");
+  printf((char *)argv[i]);
+  printf("'\r\n\r\n");
+  return abort_with_help();
+}
+
+uint8_t abort_with_missing_file_name_msg() {
+  printf("Missing filename.\r\n\r\n");
+  return abort_with_help();
+}
+
+void process_cli_arguments(const int argc, const char **argv) {
+  for (uint8_t i = 1; i < argc;) {
+    const uint8_t current_i = i;
+
+    i = arg_help_msg(i, argv);
+    if (current_i != i)
+      continue;
+
+    i = arg_file_name(i, argv);
+    if (current_i != i)
+      continue;
+
+    abort_with_invalid_arg_msg(i, argv);
+  }
+
+  if (pFileName == NULL)
+    abort_with_missing_file_name_msg();
+}
--- msx/apps/muflash/main.c
+++ msx/apps/muflash/main.c
@@ -1,12 +1,41 @@
+#include "arguments.h"
 #include <msxdos.h>
 #include <stdio.h>
 
-extern void msxMusicEraseROM();
+extern void     msxMusicEraseROM();
+extern void     msxMusicWriteFirst16KROM();
+extern uint16_t msxMusicSetSlot();
+extern void     msxMusicRestoreSlot(uint16_t slot_assignment) __z88dk_fastcall;
+extern uint16_t spike();
+extern void     msxMusicSetPage(uint8_t p) __z88dk_fastcall;
 
-void main() {
-  printf("SPIKE flash MSX-MUSIC ROM ....\r\n");
+uint8_t __at 0x8000 buffer[1024 * 16];
+
+static uint8_t *ptr;
+static uint16_t slot_assignment;
+static FILE *   pFile;
+static size_t   length;
+
+void main(const int argc, const unsigned char **argv) {
+
+  process_cli_arguments(argc, argv);
+
+  pFile = fopen(pFileName, "rb");
+
+  if (!pFile) {
+    printf("Unable to open file '%s'\r\n", pFileName);
+    exit(1);
+  }
+
+  length = fread(buffer, 1, 16 * 1024, pFile);
+
+  fclose(pFile);
+
+  printf("Flashing %lu bytes to MSX-MUSIC onboard ROM...", (unsigned long)length);
 
   msxMusicEraseROM();
+  printf(".");
+  msxMusicWriteFirst16KROM();
 
-  printf("Done\r\n");
+  printf("\r\nFlash completed.  Please reset now.\r\n");
 }
