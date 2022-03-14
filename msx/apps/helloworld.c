bc34fba5e84980f5ee8f58a2ab407b7889b3b9e6
Dean Netherton <dean.netherton@gmail.com>
2022-03-14 16:02:01 +1100

    usb-storage: work in progress for driver

--- msx/apps/helloworld.c
+++ msx/apps/helloworld.c
@@ -5,6 +5,31 @@
 
 uint8_t fn(const uint8_t y) { return y + 10; }
 
+void xx(uint8_t* a) {
+  *a = *a + 1;
+}
+
+uint8_t test(uint8_t aa) {
+  uint8_t a;
+  // uint8_t b;
+  // uint8_t c;
+  // uint8_t d;
+  // uint8_t e;
+  a = 1+aa;
+  // b = 2+aa;
+  // c = 3+aa;
+  // d = 4+aa;
+  // e = 5 + aa;
+
+  for(uint8_t x = 1; x < 100; x++) {
+    xx(&a);
+    // xx(&b);
+    // xx(&c);
+    printf("%d", a);
+  }
+
+  return a;
+}
 void main(const int argc, const char *argv[]) {
   (void)argc;
   printk("     Hello World 3: %d, %d, %d\r\n", JIFFY, CSRX, CSRY);
@@ -13,4 +38,6 @@ void main(const int argc, const char *argv[]) {
   const uint8_t rowAbove = CSRY - 1;
   msxbiosPosit(1, rowAbove);
   printk("%s %d\r\n", argv[1], argc);
+
+  test(1);
 }
