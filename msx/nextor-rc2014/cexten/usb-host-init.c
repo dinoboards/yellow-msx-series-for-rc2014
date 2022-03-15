3e237b2de9ca1e2cbebd1ea39129001ae144107c
Dean Netherton <dean.netherton@gmail.com>
2022-03-15 21:03:17 +1100

    ch376: progressing of converting driver to c code

--- msx/nextor-rc2014/cexten/usb-host-init.c
+++ msx/nextor-rc2014/cexten/usb-host-init.c
@@ -2,20 +2,118 @@
 #include <delay.h>
 #include "print.h"
 #include "work-area.h"
+#include <stdbool.h>
 
 __sfr __at 0x84 CH376_DATA_PORT;
 __sfr __at 0x85 CH376_COMMAND_PORT;
 
-#define CH_CMD_RESET_ALL  0x05
+#define CH_CMD_GET_IC_VER   0x01
+#define CH_CMD_RESET_ALL    0x05
+#define CH_CMD_CHECK_EXIST  0x06
+#define CH_CMD_SET_USB_MODE 0x15
+#define CH_CMD_SET_RETRY    0x0B
 
-inline void usb_reset() {
+
+#define CH_MODE_HOST_RESET  7
+#define CH_MODE_HOST        6
+
+// return codes
+#define CH_ST_RET_SUCCESS         0x51
+#define CH_ST_RET_ABORT           0x5F
+
+inline void ch376_reset() {
   CH376_COMMAND_PORT = CH_CMD_RESET_ALL;
-  delay(6);
+  delay(60);
 }
 
-void usb_host_init() {
-  void *p = get_work_area();
+/* =============================================================================
+
+  Check if the USB host controller hardware is operational
+  
+  Returns:
+    1 is operation, 0 if not
+
+============================================================================= */
+inline uint8_t ch376_test() {
+  CH376_COMMAND_PORT = CH_CMD_CHECK_EXIST;
+  CH376_DATA_PORT = (uint8_t)~0x34;
+  if (CH376_DATA_PORT != 0x34)
+    return false;
+
+  CH376_COMMAND_PORT = CH_CMD_CHECK_EXIST;
+  CH376_DATA_PORT = (uint8_t)~0x89;
+  return (CH376_DATA_PORT == 0x89);
+}
+
+/* =============================================================================
+
+  Retrieve the CH376 chip version
+
+  Returns:
+    The chip version
+============================================================================= */
+inline uint8_t ch376_get_firmware_version() {
+  CH376_COMMAND_PORT = CH_CMD_GET_IC_VER;
+  return CH376_DATA_PORT & 0x1f;
+}
+
+/* =============================================================================
+
+  Set the CH376 USB MODE
+
+  Returns:
+    0 -> OK, 1 -> ERROR
+============================================================================= */
+uint8_t ch376_set_usb_mode(uint8_t mode) __z88dk_fastcall {
+  CH376_COMMAND_PORT = CH_CMD_SET_USB_MODE;
+  CH376_DATA_PORT = mode;
+
+  uint8_t count = 255;
+  while( CH376_DATA_PORT != CH_ST_RET_SUCCESS && --count != 0)
+    ;
+
+  return count != 0;
+}
+
+void hw_configure_nak_retry() {
+  CH376_COMMAND_PORT = CH_CMD_SET_RETRY;
+  CH376_DATA_PORT = 0x25;
+  CH376_DATA_PORT = 0x8F;   // Retry NAKs indefinitely (default)
+}
+
+uint8_t usb_host_bus_reset() {
+  ch376_set_usb_mode(CH_MODE_HOST);
+  delay(60/4);
+
+  ch376_set_usb_mode(CH_MODE_HOST_RESET);
+  delay(60/2);
+
+  ch376_set_usb_mode(CH_MODE_HOST);
+  delay(60/4);
+
+  hw_configure_nak_retry();
+
+  return true;
+}
+
+uint8_t usb_host_init() {
+  work_area *p = get_work_area();
   xprintf("usb_host_init %p\r\n", p);
 
-  usb_reset();
+  ch376_reset();
+
+  if (!ch376_test()) {
+    xprintf("CH376:           NOT PRESENT\r\n");
+    return false;
+  }
+
+  p->ch376.present = true;
+  const uint8_t ver = ch376_get_firmware_version();
+  xprintf("CH376:           PRESENT (VER %d)\r\n", ver);
+
+  if (!usb_host_bus_reset())
+    return false;
+
+
+  return true;
 }
--- msx/nextor-rc2014/cexten/work-area.h
+++ msx/nextor-rc2014/cexten/work-area.h
@@ -1,2 +1,21 @@
 
-extern void* get_work_area();
+
+// 	STRUCT WRKAREA
+// BASE:					; Offset to the base of the data structure
+// STATUS:			db 		; bit 0 = CH376s present, bit 1 = initialised, bit 2 = USB device present, bit 3 = USB device mounted, bit 5 = DSK changed, bit 7 = Flash disk present
+
+typedef struct _ch376_work_area {
+  // uint8_t status;
+  uint8_t present : 1;
+} ch376_work_area;
+
+typedef struct _work_area {
+  uint8_t   read_count;             // COUNT OF SECTORS TO BE READ
+  uint16_t  index;                  // sector number to be read
+  uint8_t*  dest;                   // destination write address
+  uint8_t   read_count_requested;   // number of sectors requested
+  uint8_t   present;                // BIT FIELD FOR DETECTED DEVICES (BIT 0 -> COMPACTFLASH/IDE, BIT 1-> MSX-MUSIC NOR FLASH)
+  ch376_work_area   ch376;
+} work_area;
+
+extern work_area* get_work_area();
