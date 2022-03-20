eed7aee371495dfeff5fa651738830f450b41758
Dean Netherton <dnetherton@dius.com.au>
2022-03-20 12:27:30 +1100

    ch376: progressing on converting driver to c

--- msx/nextor-rc2014/cexten/debuggin.h
+++ msx/nextor-rc2014/cexten/debuggin.h
@@ -1,14 +1,14 @@
 #ifndef __DEBUGGIN
 #define __DEBUGGIN
 
-#include <stdlib.h>
 #include "print.h"
 #include "work-area.h"
+#include <stdlib.h>
 
-extern void logConfig(const config_descriptor* const p);
+extern void logConfig(const config_descriptor *const p);
 
-extern void logDevice(const device_descriptor* const p);
+extern void logDevice(const device_descriptor *const p);
 
-extern void logUsbDevice(const _usb_device_info* const info);
+extern void logUsbDevice(const _usb_device_info *const info);
 
 #endif
