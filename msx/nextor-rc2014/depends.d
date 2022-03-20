03fbc3cad1731d8848d33794db6cff0d562663b9
Dean Netherton <dnetherton@dius.com.au>
2022-03-20 21:14:21 +1100

    ch376: progressing on converting driver to c

--- msx/nextor-rc2014/depends.d
+++ msx/nextor-rc2014/depends.d
@@ -1,11 +1,12 @@
 
 ./bin/print.c.asm: cexten/print.c cexten/print.h
+./bin/scsi.c.asm: cexten/scsi.c cexten/scsi.h
 ./bin/spike.c.asm: cexten/spike.c
 ./bin/debuggin.c.asm: cexten/debuggin.c cexten/debuggin.h cexten/print.h \
  cexten/work-area.h
 ./bin/usb-host-init.c.asm: cexten/usb-host-init.c cexten/debuggin.h \
- cexten/print.h cexten/work-area.h ../apps/libraries/delay/delay.h \
- ../apps/libraries/msxbios/system_vars.h
+ cexten/print.h cexten/work-area.h cexten/scsi.h \
+ ../apps/libraries/delay/delay.h ../apps/libraries/msxbios/system_vars.h
 ./bin/chgbnk.o: ./chgbnk.asm
 ./bin/rcembdrv.o: ./rcembdrv.asm
 ./bin/cfdrv.o: ./cfdrv.asm
