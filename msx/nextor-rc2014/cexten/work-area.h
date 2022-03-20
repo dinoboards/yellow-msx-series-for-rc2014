e33994d7ce75571e467fa2c24ee1adc20366e1d6
Dean Netherton <dnetherton@dius.com.au>
2022-03-20 12:27:30 +1100

    ch376: progressing on converting driver to c

--- msx/nextor-rc2014/cexten/work-area.h
+++ msx/nextor-rc2014/cexten/work-area.h
@@ -12,7 +12,8 @@ typedef struct __scsi_command_status_wrapper {
 
 // 	STRUCT WRKAREA
 // BASE:					; Offset to the base of the data structure
-// STATUS:			db 		; bit 0 = CH376s present, bit 1 = initialised, bit 2 = USB device present, bit 3 = USB device mounted, bit 5 = DSK changed, bit 7 = Flash disk present
+// STATUS:			db 		; bit 0 = CH376s present, bit 1 = initialised, bit 2 = USB device present, bit
+// 3 = USB device mounted, bit 5 = DSK changed, bit 7 = Flash disk present
 
 typedef struct __usb_device_info {
   uint8_t device_address;
@@ -42,75 +43,75 @@ typedef struct __hub_device_info {
 typedef struct __scsi_devinfo {
   uint8_t tag;
   uint8_t max_luns;
-  uint8_t buffer[0x24];       // longest response (inquiry) we want to absorb during init
-  uint8_t csw[sizeof(_scsi_command_status_wrapper)];//			ds	_scsi_command_status_wrapper
+  uint8_t buffer[0x24];                              // longest response (inquiry) we want to absorb during init
+  uint8_t csw[sizeof(_scsi_command_status_wrapper)]; //			ds	_scsi_command_status_wrapper
   uint8_t vendorid[8];
-	uint8_t ignored1;
+  uint8_t ignored1;
   uint8_t productid[16];
-	uint8_t ignored2;
+  uint8_t ignored2;
   uint8_t productrev[8];
   uint8_t ignored3;
 } _scsi_devinfo;
 
 typedef struct _usb_descriptor_block {
-  uint8_t code;
-  uint8_t dat1;
-  uint8_t dat2;
-  uint8_t dat3;
-  uint8_t dat4;
-  uint8_t dat5;
+  uint8_t  code;
+  uint8_t  dat1;
+  uint8_t  dat2;
+  uint8_t  dat3;
+  uint8_t  dat4;
+  uint8_t  dat5;
   uint16_t length;
 } usb_descriptor_block;
 
 typedef struct __usb_descriptor_blocks {
-  usb_descriptor_block  cmd_get_device_descriptor;
-  usb_descriptor_block  cmd_set_address;
-  usb_descriptor_block  cmd_set_configuration;
-  usb_descriptor_block  cmd_get_string;
-  usb_descriptor_block  cmd_get_config_descriptor;
-  usb_descriptor_block  cmd_set_interface;
-  usb_descriptor_block  cmd_set_packet_filter;
-  usb_descriptor_block  cmd_set_idle;
-  usb_descriptor_block  cmd_set_protocol;
-  usb_descriptor_block  reserved[3];
-  usb_descriptor_block  cmd_get_hub_descriptor;
-  usb_descriptor_block  cmd_set_hub_port_feature;
-  usb_descriptor_block  cmd_get_hub_port_status;
-  usb_descriptor_block  cmd_get_max_luns;
-  usb_descriptor_block  cmd_mass_storage_reset;
+  usb_descriptor_block cmd_get_device_descriptor;
+  usb_descriptor_block cmd_set_address;
+  usb_descriptor_block cmd_set_configuration;
+  usb_descriptor_block cmd_get_string;
+  usb_descriptor_block cmd_get_config_descriptor;
+  usb_descriptor_block cmd_set_interface;
+  usb_descriptor_block cmd_set_packet_filter;
+  usb_descriptor_block cmd_set_idle;
+  usb_descriptor_block cmd_set_protocol;
+  usb_descriptor_block reserved[3];
+  usb_descriptor_block cmd_get_hub_descriptor;
+  usb_descriptor_block cmd_set_hub_port_feature;
+  usb_descriptor_block cmd_get_hub_port_status;
+  usb_descriptor_block cmd_get_max_luns;
+  usb_descriptor_block cmd_mass_storage_reset;
 } _usb_descriptor_blocks;
 
 typedef struct _ch376_work_area {
-  uint8_t               present : 1;
-  uint8_t               initialised : 1;
-  uint8_t               max_device_address;
-  _usb_device_info      usb_device_info;
-  _usb_device_info      storage_device_info;
-  _search_device_info   search_device_info;
-  _hub_device_info		  hub_device_info;
-  _scsi_devinfo         scsi_device_info;
-  uint8_t               usb_descriptor[140];    // memory area to hold the usb device+config descriptor of the current interrogated device
+  uint8_t             present : 1;
+  uint8_t             initialised : 1;
+  uint8_t             max_device_address;
+  _usb_device_info    usb_device_info;
+  _usb_device_info    storage_device_info;
+  _search_device_info search_device_info;
+  _hub_device_info    hub_device_info;
+  _scsi_devinfo       scsi_device_info;
+  uint8_t usb_descriptor[140]; // memory area to hold the usb device+config descriptor of the current interrogated device
 
   _usb_descriptor_blocks usb_descriptor_blocks;
 
   // usb_descriptor_block  cmd_get_device_descriptor;
   // usb_descriptor_block  cmd_set_address;
 
-// USB_DESCRIPTORS		ds	USB_DESCRIPTORS_END - USB_DESCRIPTORS_START ; **
+  // USB_DESCRIPTORS		ds	USB_DESCRIPTORS_END - USB_DESCRIPTORS_START ; **
 
-// NXT_DIRECT		ds	NXT_DIRECT_END - NXT_DIRECT_START
+  // NXT_DIRECT		ds	NXT_DIRECT_END - NXT_DIRECT_START
 } ch376_work_area;
 
 typedef struct _work_area {
-  uint8_t   read_count;             // COUNT OF SECTORS TO BE READ
-  uint16_t  index;                  // sector number to be read
-  uint8_t*  dest;                   // destination write address
-  uint8_t   read_count_requested;   // number of sectors requested
-  uint8_t   present;                // BIT FIELD FOR DETECTED DEVICES (BIT 0 -> COMPACTFLASH/IDE, BIT 1-> MSX-MUSIC NOR FLASH)
-  ch376_work_area   ch376;
+  uint8_t         read_count;           // COUNT OF SECTORS TO BE READ
+  uint16_t        index;                // sector number to be read
+  uint8_t *       dest;                 // destination write address
+  uint8_t         read_count_requested; // number of sectors requested
+  uint8_t         present; // BIT FIELD FOR DETECTED DEVICES (BIT 0 -> COMPACTFLASH/IDE, BIT 1-> MSX-MUSIC NOR FLASH)
+  ch376_work_area ch376;
 } work_area;
 
-extern work_area* get_work_area();
+extern work_area *get_work_area();
 
 typedef struct _device_descriptor {
   uint8_t  bLength;
