1c7d397ec4221f88bcbc9648a8853fd82ea4ad12
Dean Netherton <dnetherton@dius.com.au>
2022-04-03 09:49:44 +1000

    ch376: removed old version of c code 'cexten'

--- msx/nextor-rc2014/depends.d
+++ msx/nextor-rc2014/depends.d
@@ -26,24 +26,6 @@
 ./bin/debuggin.c.asm: usbfdd/debuggin.c usbfdd/debuggin.h usbfdd/nextor.h \
  usbfdd/ufi.h usbfdd/ch376.h usbfdd/usb.h usbfdd/work-area.h \
  usbfdd/print.h
-./bin/usb-dev-read.c.asm: cexten/usb-dev-read.c cexten/ch376.h cexten/scsi.h \
- cexten/work-area.h ../apps/libraries/delay/delay.h \
- ../apps/libraries/msxbios/system_vars.h cexten/print.h
-./bin/usb-lun-info.c.asm: cexten/usb-lun-info.c cexten/ch376.h cexten/print.h \
- cexten/scsi.h cexten/work-area.h
-./bin/print.c.asm: cexten/print.c cexten/print.h
-./bin/scsi.c.asm: cexten/scsi.c cexten/scsi.h cexten/work-area.h
-./bin/usb-dev-write.c.asm: cexten/usb-dev-write.c cexten/ch376.h cexten/scsi.h \
- cexten/work-area.h ../apps/libraries/delay/delay.h \
- ../apps/libraries/msxbios/system_vars.h cexten/print.h
-./bin/spike.c.asm: cexten/spike.c
-./bin/usb-dev-info.c.asm: cexten/usb-dev-info.c cexten/work-area.h
-./bin/usb-dev-status.c.asm: cexten/usb-dev-status.c cexten/work-area.h
-./bin/debuggin.c.asm: cexten/debuggin.c cexten/debuggin.h cexten/print.h \
- cexten/work-area.h
-./bin/usb-host-init.c.asm: cexten/usb-host-init.c cexten/ch376.h \
- cexten/debuggin.h cexten/print.h cexten/work-area.h cexten/scsi.h \
- ../apps/libraries/delay/delay.h ../apps/libraries/msxbios/system_vars.h
 ./bin/chgbnk.o: ./chgbnk.asm
 ./bin/rc2014dr.inc.o: ./rc2014dr.inc.asm
 ./bin/rcembdrv.o: ./rcembdrv.asm
@@ -58,6 +40,3 @@
 ./bin/musicdriver.o: ./musicdriver.asm
 ./bin/rc2014dr.o: ./rc2014dr.asm
 ./bin/embinc.o: ./embinc.asm
-./bin/print.o: ./cexten/print.asm ./cexten/asm.inc
-./bin/crt.o: ./cexten/crt.asm
-./bin/work-area.o: ./cexten/work-area.asm ./cexten/asm.inc
