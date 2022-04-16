721762666ab2ddf7fed69a42b287f3c78ba1377b
Dean Netherton <dnetherton@dius.com.au>
2022-04-16 20:05:02 +1000

    ch376: change some control-transfer operations to use functions of ch376 - greater compatibility

--- msx/nextor-rc2014/usbfdd/hw.h
+++ msx/nextor-rc2014/usbfdd/hw.h
@@ -12,20 +12,14 @@ extern usb_error hw_control_transfer(const setup_packet *const cmd_packet,
                                      const uint8_t             device_address,
                                      const uint8_t             max_packet_size);
 
-extern usb_error
-hw_get_description(const uint8_t device_address, const uint8_t max_packet_size, device_descriptor *const buffer);
-
-extern usb_error hw_get_description_short(device_descriptor *const buffer);
+extern usb_error hw_get_description(device_descriptor *const buffer);
 
 extern usb_error hw_get_config_descriptor(config_descriptor *const buffer,
                                           const uint8_t            config_index,
-                                          const uint8_t            max_packet_size,
                                           const uint8_t            buffer_size,
                                           const uint8_t            device_address);
 
 extern usb_error hw_set_configuration(const device_config *const config);
-
-extern usb_error hw_set_address(const uint8_t device_address, const uint8_t max_packet_size);
 
 extern usb_error hw_data_in_transfer(uint8_t *             buffer,
                                      const uint16_t        buffer_size,
@@ -36,5 +30,9 @@ extern usb_error hw_data_out_transfer(const uint8_t *       buffer,
                                       uint16_t              buffer_size,
                                       const uint8_t         device_address,
                                       endpoint_param *const endpoint);
+
+extern usb_error hw_get_desc_spike(device_descriptor *const buffer);
+
+inline usb_error hw_set_address(const uint8_t device_address) { return ch_control_transfer_set_address(device_address); }
 
 #endif
