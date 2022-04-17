031e6bc7ee8468aa3b685f2d6adfb24edaf1df24
Dean Netherton <dnetherton@dius.com.au>
2022-04-17 19:10:14 +1000

    ch376: fix issue with unknown devices found during hub enumeration

--- msx/nextor-rc2014/usbfdd/usb-lun-info-scsi.c
+++ msx/nextor-rc2014/usbfdd/usb-lun-info-scsi.c
@@ -2,8 +2,6 @@
 #include "usb-lun-info-ufi.h"
 #include <stdlib.h>
 
-#include "print.h"
-
 uint8_t usb_lun_info_scsi(storage_device_config *const dev, const uint8_t lun, nextor_lun_info *const info) {
   scsi_read_capacity_result response;
 
