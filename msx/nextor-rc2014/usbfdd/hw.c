5eb4b403821d4a37edc22ae1df0cd6a78b3ac18a
Dean Netherton <dnetherton@dius.com.au>
2022-04-17 12:49:32 +1000

    ch376: refactor hw - optimising code size

--- msx/nextor-rc2014/usbfdd/hw.c
+++ msx/nextor-rc2014/usbfdd/hw.c
@@ -30,8 +30,7 @@ usb_error hw_control_transfer(const setup_packet *const cmd_packet,
     return USB_ERR_OTHER;
   }
 
-  ch_command(CH_CMD_SET_USB_ADDR);
-  CH376_DATA_PORT = device_address;
+  ch_set_usb_address(device_address);
 
   ch_write_data((const uint8_t *)cmd_packet, sizeof(setup_packet));
   ch_issue_token_setup();
@@ -121,8 +120,7 @@ usb_error hw_data_in_transfer(uint8_t *             buffer,
                               const uint8_t         device_address,
                               endpoint_param *const endpoint) {
 
-  ch_command(CH_CMD_SET_USB_ADDR);
-  CH376_DATA_PORT = device_address;
+  ch_set_usb_address(device_address);
 
   return ch_data_in_transfer(buffer, buffer_size, endpoint);
 }
@@ -143,8 +141,7 @@ usb_error hw_data_out_transfer(const uint8_t *       buffer,
                                const uint8_t         device_address,
                                endpoint_param *const endpoint) {
 
-  ch_command(CH_CMD_SET_USB_ADDR);
-  CH376_DATA_PORT = device_address;
+  ch_set_usb_address(device_address);
 
   return ch_data_out_transfer(buffer, buffer_size, endpoint);
 }
