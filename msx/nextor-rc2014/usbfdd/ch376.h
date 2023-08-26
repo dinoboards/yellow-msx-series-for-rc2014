2b54029268c2a7659d0f337556cdeec6447cc8da
Dean Netherton <dean.netherton@gmail.com>
2023-08-27 09:37:18 +1000

    ch376: adjusted port numbers to current hardware design

--- msx/nextor-rc2014/usbfdd/ch376.h
+++ msx/nextor-rc2014/usbfdd/ch376.h
@@ -79,8 +79,8 @@ typedef enum _ch376_pid {
   CH_PID_OUT   = DEF_USB_PID_OUT
 } ch376_pid;
 
-__sfr __at 0x84 CH376_DATA_PORT;
-__sfr __at 0x85 CH376_COMMAND_PORT;
+__sfr __at 0x88 CH376_DATA_PORT;
+__sfr __at 0x89 CH376_COMMAND_PORT;
 
 extern void delay_short();
 extern void delay_medium();
