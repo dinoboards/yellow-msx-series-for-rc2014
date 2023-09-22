1ce17156eb3aab8e8869a21fb559486b72b14054
Dean Netherton <dean.netherton@gmail.com>
2023-09-23 09:27:33 +1000

    usbdrv: export usb state structure size from c code to nextor-rc2014 assembly

--- msx/nextor-rc2014/rc2014dr.inc.asm
+++ msx/nextor-rc2014/rc2014dr.inc.asm
@@ -1,47 +1,17 @@
 
 	include	embinc.asm
 
-	STRUCT endpoint_paramx
-BASE:					; Offset to the base of the data structure
-number_toggle			db
-max_packet_size			db
-	ENDS
-
-	STRUCT device_configx
-BASE:
-type				db
-max_packet_size			db
-interface_number		db
-tag				db
-address				db
-bulk_out_endpoint		endpoint_paramx
-bulk_in_endpoint		endpoint_paramx
-intr_in_endpoint		endpoint_paramx
-	ENDS
-
-	STRUCT usb_work_areax
-BASE:					; Offset to the base of the data structure
-storage_device1			device_configx
-storage_device2			device_configx
-storage_device3			device_configx
-storage_device4			device_configx
-hub_config			device_configx
-cdc_config			device_configx
-printer_config			device_configx
-next_storage_device_index	db
-printer_time_throttle_flag	db
-	ENDS
+	include "./bin/exports.asm"
 
 	STRUCT ST_WRKAREA
-RD_CNT		DB	0	; COUNT OF SECTORS TO BE READ
-INDX_LW		DB	1	; SECTOR TO BE READ
-INDX_HI		DB	2	; SECTOR TO BE READ 	; MUST BE LESS THAN 65536
-DEST_LW		DB	3	; SECTOR DESTINATION ADDRESS (LOW)
-DEST_HI		DB	4	; (HIGH)
-RD_CND_RQ	DB	5	; NUMBER OF SECTORS REQUESTED
-PRESENT		DB	6	; BIT FIELD FOR DETECTED DEVICES (BIT 0 -> COMPACTFLASH/IDE, BIT 1-> MSX-MUSIC NOR FLASH)
-USB		usb_work_areax
-		; DS	16	; reserve
+RD_CNT		DS	1	; COUNT OF SECTORS TO BE READ
+INDX_LW		DS	1	; SECTOR TO BE READ
+INDX_HI		DS	1	; SECTOR TO BE READ 	; MUST BE LESS THAN 65536
+DEST_LW		DS	1	; SECTOR DESTINATION ADDRESS (LOW)
+DEST_HI		DS	1	; (HIGH)
+RD_CND_RQ	DS	1	; NUMBER OF SECTORS REQUESTED
+PRESENT		DS	1	; BIT FIELD FOR DETECTED DEVICES (BIT 0 -> COMPACTFLASH/IDE, BIT 1-> MSX-MUSIC NOR FLASH)
+USB		DS	_size_of_usb_state
 
 	ENDS
 
