db5a3517ba127c0bea42dd8e5a613a3f84def7b0
Dean Netherton <dean.netherton@gmail.com>
2023-09-27 20:48:03 +1000

    usbdrv: revert ch376 ports back to 0x88-0x89

--- msx/apps/libraries/usb/ch376.h
+++ msx/apps/libraries/usb/ch376.h
@@ -114,8 +114,8 @@ typedef struct {
 
 typedef enum _ch376_pid { CH_PID_SETUP = DEF_USB_PID_SETUP, CH_PID_IN = DEF_USB_PID_IN, CH_PID_OUT = DEF_USB_PID_OUT } ch376_pid;
 
-__sfr __at 0xB8 CH376_DATA_PORT;
-__sfr __at 0xB9 CH376_COMMAND_PORT;
+__sfr __at 0x88 CH376_DATA_PORT;
+__sfr __at 0x89 CH376_COMMAND_PORT;
 
 extern void delay_short();
 extern void delay_medium();
