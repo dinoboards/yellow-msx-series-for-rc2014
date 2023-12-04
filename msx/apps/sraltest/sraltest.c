2c1df890617c35becef16e65a81df34667d3e11c
Dean Netherton <dean.netherton@gmail.com>
2023-12-04 18:24:39 +1100

    sraltest: revert back to simple loopback test

--- msx/apps/sraltest/sraltest.c
+++ msx/apps/sraltest/sraltest.c
@@ -12,7 +12,7 @@
 
 uint32_t wanted_baud_rates[] = {300, 1200, 2400, 4800, 9600, 19200, 38400, 56700, 115200, 230400, 460800, 921600};
 
-#define BUF_SIZE 256
+#define BUF_SIZE 128
 
 uint8_t port_number = 0;
 uint8_t serial_write_temp_buffer[256];
@@ -90,8 +90,7 @@ uint8_t main(const int argc, const char *const argv[]) {
   sio_diagnostic_t d;
   memset(&d, 0, sizeof(d));
 
-  uint8_t result;
-  // uint16_t  buffer_size = BUF_SIZE;
+  uint8_t  result;
   uint8_t  buffer[BUF_SIZE];
   uint8_t  id = 0;
   uint16_t read_count;
@@ -120,93 +119,37 @@ uint8_t main(const int argc, const char *const argv[]) {
   result = serial_set_protocol(port_number, SERIAL_PARITY_NONE | SERIAL_STOPBITS_1 | SERIAL_BITS_8 | SERIAL_BREAK_OFF);
   printf("(%02X): Set protocol %04X\r\n", result, SERIAL_PARITY_NONE | SERIAL_STOPBITS_1 | SERIAL_BITS_8 | SERIAL_BREAK_OFF);
 
-  result = serial_purge_buffers(1);
+  result = serial_purge_buffers(port_number);
   printf("(%02X): Purge buffers\r\n", result);
 
   uint8_t i = 0;
-  if (msxbiosBreakX())
-    return 0;
+  while (true) {
+    if (msxbiosBreakX())
+      return 0;
 
-  for (uint16_t buf_index = 0; buf_index < BUF_SIZE; buf_index++)
-    buffer[buf_index] = id++;
+    for (uint16_t buf_index = 0; buf_index < BUF_SIZE; buf_index++)
+      buffer[buf_index] = id++;
 
-  uint16_t pending_bytes = serial_get_rx_buffer_size(port_number);
-  printf("pending: %04X\r\n", pending_bytes);
+    result = serial_write(port_number, buffer, BUF_SIZE);
+    printf("(%02X): serial_write %d bytes\r\n", result, BUF_SIZE);
 
-  sio_diagnostic_t t;
-  serial_get_diagnostics(port_number, &t);
-  printf("sio_buf: %04X\r\n", t.sio_buf);
-  printf("sio_buf_head: %04X\r\n", t.sio_buf_head);
-  printf("sio_buf_tail: %04X\r\n", t.sio_buf_tail);
-  printf("sio_data_count: %04X\r\n", t.sio_data_count);
-  printf("ch: %04X\r\n", t.ch);
+    uint16_t pending_bytes = serial_get_rx_buffer_size(port_number);
+    printf("pending: %04X\r\n", pending_bytes);
 
-  result = serial_write_string("ate0\r\n");
-  printf("(%02X): serial_write_string: ate0\r\n", result);
-  if (result)
-    return result;
+    sio_diagnostic_t t;
+    serial_get_diagnostics(port_number, &t);
+    printf("sio_buf: %04X\r\n", t.sio_buf);
+    printf("sio_buf_head: %04X\r\n", t.sio_buf_head);
+    printf("sio_buf_tail: %04X\r\n", t.sio_buf_tail);
+    printf("sio_data_count: %04X\r\n", t.sio_data_count);
+    printf("ch: %04X\r\n", t.ch);
 
-  read_count = 100;
-  memset(buffer, 0, sizeof(buffer));
-  result = serial_demand_read(port_number, buffer, &read_count, 1000);
-  printf("(%02X): serial_read returned %d bytes. \"%s\" \r\n", result, read_count, buffer);
+    read_count = BUF_SIZE;
+    memset(buffer, 0, sizeof(buffer));
+    result = serial_demand_read(port_number, buffer, &read_count, 1000);
+    printf("(%02X): serial_demand_read returned %d bytes.\r\n", result, read_count);
 
-  result = serial_write_string("at+baud=9600\r\n");
-  printf("(%02X): serial_write_string at+baud=9600\r\n", result);
-  if (result)
-    return result;
-
-  result = serial_set_baudrate(port_number, 9600L);
-  printf("(%02X): Set baudrate: %ld\r\n", result, 9600L);
-
-  read_count = 4;
-  memset(buffer, 0, sizeof(buffer));
-  result = serial_demand_read(port_number, buffer, &read_count, 1000);
-  printf("(%02X): serial_read returned %d bytes. \"%s\" \r\n", result, read_count, buffer);
-
-  atok();
-  atok();
-  atok();
-  atok();
-
-  result = serial_write_string("at+wgethttp://192.168.86.182:8080/bob.txt\r\n");
-  printf("(%02X): serial_write_string\r\n", result);
-  if (result)
-    return result;
-
-  read_count = 4;
-  memset(buffer, 0, sizeof(buffer));
-  result = serial_demand_read(port_number, buffer, &read_count, 1000);
-  printf("(%02X): serial_read returned %d bytes. \"%s\" \r\n", result, read_count, buffer);
-
-  result = serial_write_char('C');
-  printf("(%02X): serial_write_char\r\n", result);
-
-  pending_bytes = serial_get_rx_buffer_size(port_number);
-  printf("pending: %04X\r\n", pending_bytes);
-
-  result = serial_read_char(buffer);
-  printf("(%02X): serial_read_char returned %c\r\n", result, buffer[0]);
-
-  pending_bytes = serial_get_rx_buffer_size(port_number);
-  printf("pending: %04X\r\n", pending_bytes);
-
-  memset(buffer, 0, sizeof(buffer));
-  read_count = 160;
-  result     = serial_read(port_number, buffer, &read_count);
-  printf("(%02X): serial_deman_read returned %d bytes.  Expect 132 bytes\r\n", result, read_count);
-
-  for (uint16_t i = 0; i < read_count; i++)
-    printf("%02X ", buffer[i]);
-
-  printf("\r\n");
-
-  serial_get_diagnostics(port_number, &t);
-  printf("sio_buf: %04X\r\n", t.sio_buf);
-  printf("sio_buf_head: %04X\r\n", t.sio_buf_head);
-  printf("sio_buf_tail: %04X\r\n", t.sio_buf_tail);
-  printf("sio_data_count: %04X\r\n", t.sio_data_count);
-  printf("ch: %04X\r\n", t.ch);
-
-  return 0;
+    for (uint16_t buf_index = 0; buf_index < read_count; buf_index++)
+      printf("%02X ", buffer[buf_index]);
+  }
 }
