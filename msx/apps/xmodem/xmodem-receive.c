156ad136aaea07dccc8495d74f7b918b00da636e
Dean Netherton <dean.netherton@gmail.com>
2023-12-10 12:37:17 +1100

    xmodem.com: refined receive for retries, error handling, console monitoring

--- msx/apps/xmodem/xmodem-receive.c
+++ msx/apps/xmodem/xmodem-receive.c
@@ -3,6 +3,7 @@
 #include <extbio/serial-helpers.h>
 #include <extbio/serial.h>
 #include <libgen.h>
+#include <msxbios.h>
 #include <msxdos.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -19,6 +20,11 @@
 #define DATA_SIZE   128
 #define PACKET_SIZE (DATA_SIZE + 5)
 
+const unsigned char rotatingChar[4][5] = {STR_VERT_BAR CURSOR_MOVE_LEFT, STR_FOWARD_SLASH CURSOR_MOVE_LEFT,
+                                          STR_DASH CURSOR_MOVE_LEFT, STR_BACK_SLASH CURSOR_MOVE_LEFT};
+
+uint8_t  rotatingIndex   = 0;
+uint32_t total_file_size = 0;
 uint8_t  block[PACKET_SIZE];
 uint8_t  block_number = 1;
 uint8_t  retries      = 0;
@@ -43,33 +49,21 @@ static bool check_crc(void) {
 
 static uint8_t read_single_char(void) {
   uint16_t actual_size = 1;
-  printf("Requesting %d bytes\r\n", actual_size);
   memset(block, 0, sizeof(block));
   const uint8_t r = serial_demand_read(port_number, block, &actual_size, 3000);
-  printf("Received %d bytes\r\n", actual_size);
-  for (uint8_t i = 0; i < actual_size; i++) {
-    printf("%02X ", block[i]);
-  }
 
   return r == 0;
 }
 
 static uint8_t read_block(void) {
-  uint16_t actual_size = PACKET_SIZE - 1;
-  printf("Requesting %d bytes\r\n", actual_size);
-  const uint8_t r = serial_demand_read(port_number, block + 1, &actual_size, 3000);
-  printf("Received %d bytes\r\n", actual_size);
-  for (uint8_t i = 0; i < actual_size; i++) {
-    printf("%02X ", block[i]);
-  }
-
+  uint16_t      actual_size = PACKET_SIZE - 1;
+  const uint8_t r           = serial_demand_read(port_number, block + 1, &actual_size, 3000);
   return r == 0;
 }
 
 static void create_temp_file(void) {
   strcpy(file_name_buffer, p_file_name);
-  pPath = dirname(file_name_buffer);
-  printf("%s, %s\r\n", file_name_buffer, pPath);
+  pPath       = dirname(file_name_buffer);
   p_file_name = basename(p_file_name);
   sprintf(p_temp_file_name, "%sxmdwn.tmp", pPath, p_file_name);
 
@@ -87,8 +81,17 @@ static void create_temp_file(void) {
   fptr = error_handle & 0xFF;
 }
 
+#define WRITE_BUFFER_SIZE 2048
+uint8_t  write_buffer[WRITE_BUFFER_SIZE];
+uint16_t write_buffer_length = 0;
+
 static void write_block_to_file(void) {
-  error = msxdosWriteFile(fptr, block + 3, DATA_SIZE);
+  memcpy(write_buffer + write_buffer_length, block + 3, DATA_SIZE);
+  write_buffer_length += DATA_SIZE;
+  if (write_buffer_length < WRITE_BUFFER_SIZE)
+    return;
+
+  error = msxdosWriteFile(fptr, write_buffer, WRITE_BUFFER_SIZE);
   if (error) {
     char error_description[64];
     memset(error_description, 0, sizeof(error_description));
@@ -96,26 +99,46 @@ static void write_block_to_file(void) {
     printf("%s (%02X): %s.\r\n", error_description, error, p_temp_file_name);
     exit(255);
   }
+
+  write_buffer_length = 0;
+}
+
+static void flush_write_buffer(void) {
+  if (write_buffer_length == 0)
+    return;
+
+  error = msxdosWriteFile(fptr, write_buffer, write_buffer_length);
+  if (error) {
+    char error_description[64];
+    memset(error_description, 0, sizeof(error_description));
+    msxdosExplainErrorCode(error, error_description);
+    printf("%s (%02X): %s.\r\n", error_description, error, p_temp_file_name);
+    exit(255);
+  }
+
+  write_buffer_length = 0;
 }
 
 void xmodem_receive(void) {
+  memset(write_buffer, 0, sizeof(write_buffer));
+
   create_temp_file();
 
   serial_purge_buffers(port_number);
 
+  printf("Downloading '%s': ", p_file_name);
+
   serial_write_char('C');
-  printf("Sent C\r\n");
 
   while (true) {
     if (!read_single_char()) {
     nak_and_retry:
-      printf("Purging...\r\n");
       serial_purge_buffers(port_number);
       serial_write_char(NAK);
       retries++;
 
       if (retries > MAX_RETRIES) {
-        printf("Too many retries\r\n");
+        printf(ERASE_LINE "'%s' failed to download. Too many errors\r\n", p_file_name);
         goto aborted;
       }
 
@@ -123,57 +146,56 @@ void xmodem_receive(void) {
     }
 
     if (block[0] == EOT) {
-      printf("Received EOT\r\n");
       serial_write_char(ACK);
       break;
     }
 
     if (block[0] != SOH) {
-      printf("Expected SOH.  Received %02X\r\n", block[0]);
       goto nak_and_retry;
     }
 
     read_block();
 
     if (block[0] == CAN && block[1] == CAN && block[2] == CAN) {
-      printf("Received CANs\r\n");
       serial_write_char(ACK);
       goto aborted;
     }
 
     if (block[1] < block_number && block[2] == (uint8_t)~block[1]) {
-      // received a duplicate pck - lets just move on
-      printf("Received duplicate block %d\r\n", block[1]);
+      // ignore a previously received duplicated packet
       serial_write_char(ACK);
       retries = 0;
       continue;
     }
 
     if (block[1] != block_number) {
-      printf("Expected block %d.  Received %d\r\n", block_number, block[1]);
       goto nak_and_retry;
     }
 
     if (block[2] != (uint8_t)~block_number) {
-      printf("Expected block complement %02X.  Received %02X\r\n", (uint8_t)~block_number, block[2]);
       goto nak_and_retry;
     }
 
     if (!check_crc()) {
-      printf("CRC error\r\n");
       goto nak_and_retry;
     }
 
-    printf("Received block %d OK\r\n", block_number);
+    printf(rotatingChar[rotatingIndex]);
+    rotatingIndex = (rotatingIndex + 1) & 3;
+
     write_block_to_file();
+    total_file_size += DATA_SIZE;
     serial_write_char(ACK);
     block_number++;
     retries = 0;
   }
 
+  flush_write_buffer();
   msxdosCloseFile(fptr);
   msxdosDeleteFile(p_file_name);
   msxdosRenameFile(p_file_name, p_temp_file_name);
+
+  printf(ERASE_LINE "Downloaded '%s' (%ld bytes)\r\n", p_file_name, total_file_size);
 
   return;
 
--- msx/apps/xmodem/xmodem.c
+++ msx/apps/xmodem/xmodem.c
@@ -7,9 +7,6 @@ void main(const uint16_t argc, const char *argv[]) {
   process_cli_arguments(argc, argv);
 
   if (receive_file) {
-    printf("Receiving file '");
-    printf(p_file_name);
-    printf("' on COM1\r\n");
     xmodem_receive();
     return;
   }
