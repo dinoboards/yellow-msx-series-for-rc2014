cc98f43bb645fd2fa660fe88dbf9120afcc0b97e
Dean Netherton <dean.netherton@gmail.com>
2024-02-07 08:59:13 +1100

    rc2014-extended: removed old (mostly commented out) serial driver code as its now moved to the SIO2.SYS serial driver

--- msx/rc2014-extended/rc2014-extended.asm
+++ msx/rc2014-extended/rc2014-extended.asm
@@ -2,7 +2,6 @@
 	ORG	08000H
 
 	include "msx.inc"
-	INCLUDE	"sio.inc"
 
 ; +0	ID		Put these first two bytes at 041H and 042H ("AB") to indicate that it is an additional ROM.
 ; +2	INIT		Address of the routine to call to initialize a work area or I/O ports, or run a game, etc.
@@ -19,15 +18,8 @@ ROM_HEADER:
 	DW	0
 	DS	6
 
-	; include	"segment1.inc"
 	INCLUDE "rominit.asm"
-	; INCLUDE	"fossil.asm"
-	; INCLUDE "alloc.asm"
-	; INCLUDE "sio.asm"
 	INCLUDE	"utils.asm"
-	; INCLUDE "rs232.asm"
 	include "probing.asm"
-	; include	"extended-bios.asm"
-	; include "segment1.asm"
 
 	DS	$C000 - $
